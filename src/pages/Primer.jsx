391a63c91527ddee8b7a6cc0d69e62a8f7734b7c
Harry L. <harry@spolem.net>
2025-12-05 23:52:22 -0600

    Prepare primer page

--- src/pages/Primer.jsx
+++ src/pages/Primer.jsx
@@ -1,7 +1,115 @@
+import arrowDown from "../assets/arrow_downward.svg";
 
 function Primer(){
 	return (
-		<h1 className="display-1">Primer</h1>
+		<div className="container d-flex flex-column">
+			<h1 className="text-center fw-bolder display-3 border-bottom border-3 pb-2 border-info">Primer</h1>
+			{/*	SECTION ONE*/}
+			<div className="row align-items-center justify-content-center">
+				<div className="col-sm-auto w-auto">
+					<button className="btn btn-dark border-0" type="button" data-bs-toggle="collapse" data-bs-target="#collapseQA1" aria-expanded="false" aria-controls="collapseQA1">
+						<p className="display-5">General Deck Objectives <img src={arrowDown} alt="A down arrow." /></p>
+					</button>
+				</div>
+			</div>
+			<div className="row align-items-center justify-content-center">
+				<div className="col-sm-auto">
+					<div className="collapse" id="collapseQA1">
+						<div className="card bg-dark mt-2 border-info border-2 rounded">
+							<div className="card-body bg-dark text-bg-dark">
+								<p className="card-text qa_text_limit">Despite all shifts in the metagame, a consistent and thorough Cradle-centric value pile will sustain you through thick and thin.</p>
+								<p className="card-text qa_text_limit">While the deck is slower, it still thrives in the early game by allowing you access to excess mana via mana-dorks, Thrasios activations, and our free commander.</p>
+								<p className="card-text qa_text_limit">If you enjoy accumulating massive piles of cards and mana, then comboing off under the safety of an exiled or phased out board, the Rog-Thras is for you.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/*SECTION TWO*/}
+			<div className="row align-items-center justify-content-center">
+				<div className="col-sm-auto">
+					<button className="btn btn-dark border-0" type="button" data-bs-toggle="collapse" data-bs-target="#collapseQA2" aria-expanded="false" aria-controls="collapseQA2">
+						<p className="display-5">Important Includes <img src={arrowDown} alt="A down arrow." /></p>
+					</button>
+				</div>
+			</div>
+			<div className="row align-items-center justify-content-center">
+				<div className="col-sm-auto">
+					<div className="collapse" id="collapseQA2">
+						<div className="card bg-dark mt-2 border-info border-2 rounded">
+							<div className="card-body bg-dark text-bg-dark">
+								<p className="card-text qa_text_limit">Despite all shifts in the metagame, a consistent and thorough Cradle-centric value pile will sustain you through thick and thin.</p>
+								<p className="card-text qa_text_limit">While the deck is slower, it still thrives in the early game by allowing you access to excess mana via mana-dorks, Thrasios activations, and our free commander.</p>
+								<p className="card-text qa_text_limit">If you enjoy accumulating massive piles of cards and mana, then comboing off under the safety of an exiled or phased out board, the Rog-Thras is for you.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/*SECTION THREE*/}
+			<div className="row align-items-center justify-content-center">
+				<div className="col-sm-auto">
+					<button className="btn btn-dark border-0" type="button" data-bs-toggle="collapse" data-bs-target="#collapseQA3" aria-expanded="false" aria-controls="collapseQA3">
+						<p className="display-5">Combos <img src={arrowDown} alt="A down arrow." /></p>
+					</button>
+				</div>
+			</div>
+			<div className="row align-items-center justify-content-center">
+				<div className="col-sm-auto">
+					<div className="collapse" id="collapseQA3">
+						<div className="card bg-dark mt-2 border-info border-2 rounded">
+							<div className="card-body bg-dark text-bg-dark">
+								<p className="card-text qa_text_limit">Despite all shifts in the metagame, a consistent and thorough Cradle-centric value pile will sustain you through thick and thin.</p>
+								<p className="card-text qa_text_limit">While the deck is slower, it still thrives in the early game by allowing you access to excess mana via mana-dorks, Thrasios activations, and our free commander.</p>
+								<p className="card-text qa_text_limit">If you enjoy accumulating massive piles of cards and mana, then comboing off under the safety of an exiled or phased out board, the Rog-Thras is for you.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/*SECTION FOUR*/}
+			<div className="row align-items-center justify-content-center">
+				<div className="col-sm-auto">
+					<button className="btn btn-dark border-0" type="button" data-bs-toggle="collapse" data-bs-target="#collapseQA4" aria-expanded="false" aria-controls="collapseQA4">
+						<p className="display-5">Mulligans <img src={arrowDown} alt="A down arrow." /></p>
+					</button>
+				</div>
+			</div>
+			<div className="row align-items-center justify-content-center">
+				<div className="col-sm-auto">
+					<div className="collapse" id="collapseQA4">
+						<div className="card bg-dark mt-2 border-info border-2 rounded">
+							<div className="card-body bg-dark text-bg-dark">
+								<p className="card-text qa_text_limit">Despite all shifts in the metagame, a consistent and thorough Cradle-centric value pile will sustain you through thick and thin.</p>
+								<p className="card-text qa_text_limit">While the deck is slower, it still thrives in the early game by allowing you access to excess mana via mana-dorks, Thrasios activations, and our free commander.</p>
+								<p className="card-text qa_text_limit">If you enjoy accumulating massive piles of cards and mana, then comboing off under the safety of an exiled or phased out board, the Rog-Thras is for you.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		{	/*SECTION FIVE*/}
+			<div className="row align-items-center justify-content-center">
+				<div className="col-sm-auto">
+					<button className="btn btn-dark border-0" type="button" data-bs-toggle="collapse" data-bs-target="#collapseQA5" aria-expanded="false" aria-controls="collapseQA5">
+						<p className="display-5">Turn Outline <img src={arrowDown} alt="A down arrow." /></p>
+					</button>
+				</div>
+			</div>
+			<div className="row align-items-center justify-content-center">
+				<div className="col-sm-auto pb-2">
+					<div className="collapse" id="collapseQA5">
+						<div className="card bg-dark mt-2 border-info border-2 rounded">
+							<div className="card-body bg-dark text-bg-dark">
+								<p className="card-text qa_text_limit">Despite all shifts in the metagame, a consistent and thorough Cradle-centric value pile will sustain you through thick and thin.</p>
+								<p className="card-text qa_text_limit">While the deck is slower, it still thrives in the early game by allowing you access to excess mana via mana-dorks, Thrasios activations, and our free commander.</p>
+								<p className="card-text qa_text_limit">If you enjoy accumulating massive piles of cards and mana, then comboing off under the safety of an exiled or phased out board, the Rog-Thras is for you.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
